ca914da63733585c1dcc7c84254024c96264cf9d
Lee Lemus <leelemus@gmail.com>
2020-02-23 15:14:54 -0600

    more routing practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react'
+import { Route, Link } from 'react-router-dom'
 import PoemListPage from './PoemListPage'
-import './App.css';
+import PoemPage from './PoemPage'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
+
+export default function App() {
+  return (
+    <div className='App'>
         <nav>
-          <a href='/'>Poem List</a>
+          <Link to='/'>Poem List</Link>
         </nav>
         <header>
           <h1>William Setstatespear</h1>
         </header>
         <main>
-          <PoemListPage />
+          <Route exact path='/' component={PoemListPage} />
+          <Route 
+            path='/poem/:poemId'
+            component={PoemPage}
+          />
         </main>
         <footer>
-          <p>© WilliamSetstatespear, 2018. All Rights Reserved.</p>
+          <p>© WilliamSetstatespear, 2020. All Rights Reserved.</p>
         </footer>
       </div>
-    );
-  }
+  )
 }
-
-export default App;
